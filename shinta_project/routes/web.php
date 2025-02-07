13c43a7651e9d8974ac9c2b9b3a1962d82f7a24e
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-02-07 10:32:29 +0700

    Update web.php

--- shinta_project/routes/web.php
+++ shinta_project/routes/web.php
@@ -4,10 +4,23 @@
 
     // mendefinisikan rute untuk URL '/' (root) dengan metode GET
     Route::get('/', function () {
-        // mengembalikan view 'profile'
-        return view('profile', [
-            'nama'  =>  ' Shinta Maria',
-            'nim' => '  E41231404',
-            'prodi' => 'Teknik Informatika'
-        ]);
+        return view ('Welcome');
     });
+    route::get('/foo', function () {
+        return 'hello word';
+    });
+    route::get('user/{id}', function ($id) {
+        return 'user '.$id;
+    });
+Route::get('/user', 'UserController@index');
+//Route::get ('/user', [UserController::class,'index']);
+
+
+
+            // mengembalikan view 'profile'
+        // return view('profile', [
+        //     'nama'  =>  ' Shinta Maria',
+        //     'nim' => '  E41231404',
+        //     'prodi' => 'Teknik Informatika'
+        // ]);
+    
