fb118b0cd7ac2997c877dbba877a3c53a0599a4c
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-02 20:53:09 +0700

    Acara 13-14

--- shinta_project/resources/views/backend/layouts/sidebar.blade.php
+++ shinta_project/resources/views/backend/layouts/sidebar.blade.php
@@ -4,28 +4,37 @@
 <ul class="sidebar-nav" id="sidebar-nav">
 
   <li class="nav-item">
-    <a class="nav-link " href="index.html">
+  <a href="{{ url('/dashboard') }}">
       <i class="bi bi-grid"></i>
       <span>Dashboard</span>
     </a>
   </li><!-- End Dashboard Nav -->
 
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="users-profile.html">
+      <i class="bi bi-person"></i>
+      <span>Profil</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+
   <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
-      <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+      <i class="bi bi-journal-text"></i><span>Riwayat Hidup</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
+   
     <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
       <li>
         <a href="components-alerts.html">
-          <i class="bi bi-circle"></i><span>Alerts</span>
+          <i class="bi bi-circle"></i><span>Pendidikan</span>
         </a>
       </li>
-      <li>
-        <a href="components-accordion.html">
-          <i class="bi bi-circle"></i><span>Accordion</span>
+       <li>
+        <a href="{{url('pengalaman_kerja')}}">
+          <i class="bi bi-circle"></i><span>Pengalaman Kerja</span>
         </a>
       </li>
-      <li>
+      <!--<li>
         <a href="components-badges.html">
           <i class="bi bi-circle"></i><span>Badges</span>
         </a>
@@ -84,11 +93,11 @@
         <a href="components-tooltips.html">
           <i class="bi bi-circle"></i><span>Tooltips</span>
         </a>
-      </li>
-    </ul>
+      </li>-->
+    </ul> 
   </li><!-- End Components Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
       <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
@@ -114,9 +123,9 @@
         </a>
       </li>
     </ul>
-  </li><!-- End Forms Nav -->
+  </li>End Forms Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
       <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
@@ -132,9 +141,9 @@
         </a>
       </li>
     </ul>
-  </li><!-- End Tables Nav -->
+  </li>End Tables Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
       <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
@@ -155,9 +164,9 @@
         </a>
       </li>
     </ul>
-  </li><!-- End Charts Nav -->
+  </li>End Charts Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
       <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
     </a>
@@ -178,58 +187,58 @@
         </a>
       </li>
     </ul>
-  </li><!-- End Icons Nav -->
+  </li>End Icons Nav -->
 
-  <li class="nav-heading">Pages</li>
+  <!-- <li class="nav-heading">Pages</li>
 
   <li class="nav-item">
     <a class="nav-link collapsed" href="users-profile.html">
       <i class="bi bi-person"></i>
       <span>Profile</span>
     </a>
-  </li><!-- End Profile Page Nav -->
+  </li>End Profile Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-faq.html">
       <i class="bi bi-question-circle"></i>
       <span>F.A.Q</span>
     </a>
-  </li><!-- End F.A.Q Page Nav -->
+  </li>End F.A.Q Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-contact.html">
       <i class="bi bi-envelope"></i>
       <span>Contact</span>
     </a>
-  </li><!-- End Contact Page Nav -->
+  </li>End Contact Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-register.html">
       <i class="bi bi-card-list"></i>
       <span>Register</span>
     </a>
-  </li><!-- End Register Page Nav -->
+  </li>End Register Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-login.html">
       <i class="bi bi-box-arrow-in-right"></i>
       <span>Login</span>
     </a>
-  </li><!-- End Login Page Nav -->
+  </li>End Login Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-error-404.html">
       <i class="bi bi-dash-circle"></i>
       <span>Error 404</span>
     </a>
-  </li><!-- End Error 404 Page Nav -->
+  </li>End Error 404 Page Nav -->
 
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="pages-blank.html">
       <i class="bi bi-file-earmark"></i>
       <span>Blank</span>
     </a>
-  </li><!-- End Blank Page Nav -->
+  </li>End Blank Page Nav -->
 
 </ul>
 
