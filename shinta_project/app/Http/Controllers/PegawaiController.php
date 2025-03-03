87d0319e3c0e5d7ee6f6c6c12d40c38c8a229e5e
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-03 09:58:36 +0700

    Acara 18

--- shinta_project/app/Http/Controllers/PegawaiController.php
+++ shinta_project/app/Http/Controllers/PegawaiController.php
@@ -13,11 +13,23 @@ class PegawaiController extends Controller
     }
 
     // Memproses data dari form
-    public function proses(Request $request) {
+    public function proses(Request $request){
+        $messages = [
+            'required' => 'Input :attribute wajib diisi!',
+            'min' => 'Input :attribute harus diisi minimal :min karakter!',
+            'max' => 'Input :attribute harus diisi maksimal :max karakter!',
+        ];
+    
+        $this->validate($request, [
+            'nama' => 'required|min:5|max:20',
+            'alamat' => 'required|alpha'
+        ], $messages);
+    
         $nama = $request->input('nama');
         $alamat = $request->input('alamat');
-
-        return "Nama : " . $nama . ", Alamat : " . $alamat;
+    
+        return "Nama : ".$nama.", Alamat : ".$alamat;
     }
+    
 
 }
