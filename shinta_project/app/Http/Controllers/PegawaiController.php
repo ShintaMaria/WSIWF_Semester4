8082cbf8766238acf341779cc60eebdfb6c70033
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-03 09:23:44 +0700

    Acara 17

---
+++ shinta_project/app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller
+{
+    public function index($shinta) {
+        return $shinta;
+    }
+
+    // Menampilkan halaman form
+    public function formulir() {
+        return view('formulir');
+    }
+
+    // Memproses data dari form
+    public function proses(Request $request) {
+        $nama = $request->input('nama');
+        $alamat = $request->input('alamat');
+
+        return "Nama : " . $nama . ", Alamat : " . $alamat;
+    }
+
+}
