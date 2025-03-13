33acf8b7a544ad906c32e1b332e215ba30469738
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-13 21:50:12 +0700

    Acara 21

--- shinta_project/routes/api.php
+++ shinta_project/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\ApiPendidikanController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//ACARA 21
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::controller(ApiPendidikanController::class)->group(function () {
+    Route::get('api_pendidikan', 'getAll');
+    Route::get('api_pendidikan/{id}', 'getPen');
+    Route::post('api_pendidikan', 'createPen');
+    Route::put('api_pendidikan/{id}', 'updatePen');
+    Route::delete('api_pendidikan/{id}', 'deletePen');
 });
