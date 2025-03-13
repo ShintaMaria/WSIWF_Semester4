cd81a93cc1ac1b520f26d9f404ff067b33e8a80b
ShintaMaria <158575036+ShintaMaria@users.noreply.github.com>
2025-03-13 22:09:14 +0700

    Acara 22

--- shinta_project/app/Http/Controllers/backend/ApiPendidikanController.php
+++ shinta_project/app/Http/Controllers/backend/ApiPendidikanController.php
@@ -8,10 +8,28 @@ use Illuminate\Support\Facades\Response;
 
 class ApiPendidikanController extends Controller
 {
+    //Acara 21
     public function getAll()
     {
         $pendidikan = Pendidikan::all();
         return Response::json($pendidikan, 201);
     }
+    public function getPen($id)
+{
+    $pendidikan = Pendidikan::find($id);
+
+    return Response::json($pendidikan, 200);
+}
+    //Acara 22
+    public function createPen(Request $request)
+    {
+        Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!'
+        ], 201);
+    }
+
 }
 
